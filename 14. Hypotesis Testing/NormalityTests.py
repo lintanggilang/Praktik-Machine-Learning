9df467e2629c9fe378293bd74d13cf7540e16c00
gilang <lintang.gilang01@gmail.com>
2020-07-28 21:59:57 +0700

    update

--- 14. Hypotesis Testing/NormalityTests.py
+++ 14. Hypotesis Testing/NormalityTests.py
@@ -5,7 +5,24 @@ stat, p = shapiro(data)
 
 print("start")
 
-print('stat=%.3f, p=%.3f' % (stat, p))
+print('p=%.3f' %p)
+if p > 0.05:
+	print('Probably Gaussian')
+else:
+	print('Probably not Gaussian')
+
+print("done")
+
+
+print("#")
+from scipy.stats import normaltest
+
+data2 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945, 0.121, -0.945]
+stat, p = normaltest(data2)
+
+print("start")
+
+print('p=%.3f' %p)
 if p > 0.05:
 	print('Probably Gaussian')
 else:
